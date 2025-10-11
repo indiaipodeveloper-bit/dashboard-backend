6a7cf64dfb1f2c38c3faa2dff92501ca734ed840
IPO <indiaipodeveloper@gmail.com>
2025-10-11 14:44:21 +0530

    backend

--- controllers/AdminContoller.js
+++ controllers/AdminContoller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import { Admins } from "../models/AdminSchema.js";
 import { User } from "../models/UserModel.js";
-import {setAdminAuthCookie } from "../services/AdminAuth.js";
+import { setAdminAuthCookie } from "../services/AdminAuth.js";
 const maxage = 3 * 24 * 60 * 60 * 1000;
 // <----------------------------------------------- Get Requests ------------------------------------------------------>
 
@@ -24,23 +24,17 @@ export async function GetAdminInfo(req, res) {
 // list of admins so the every admin can see the no of admins
 export async function getAllAdmins(req, res) {
   try {
-    const allAdmins = await Admins.find({email:{$ne:req.admin.email}});
+    const allAdmins = await Admins.find({ email: { $ne: req.admin.email } });
     return res.status(200).json({ admins: allAdmins });
   } catch (error) {
     return res.status(500).send("Sorry Internal Server Error !");
   }
 }
 
-
-
-
-
-
-
-export  async function getAllUsers(req, res) {
+export async function getAllUsers(req, res) {
   try {
-    const allBlogs = await User.find({});
-    return res.status(200).json({ allBlogs });
+    const AllUsers = await User.find({});
+    return res.status(200).json({ AllUsers });
   } catch (error) {
     return res.status(500).send("Sorry Internal Server Error !");
   }
@@ -60,7 +54,7 @@ export async function AdminLogin(req, res) {
       return res.status(400).send("No Admin Found");
     }
 
-    const veryfying = password == "1234"
+    const veryfying = password == "1234";
     if (!veryfying) {
       return res.status(400).send("Wrong Password");
     }
@@ -100,16 +94,22 @@ export async function AddNewAdmin(req, res) {
   }
 }
 
-
 export async function AddUser(req, res) {
   try {
-    const { name, email, password, phone, gender,isActive } = req.body;
+    const { name, email, password, phone, gender, isActive } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).send("User Already Exist");
     }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
     const user = await User.create({
-      name,email,password,phone,gender,isActive
+      name,
+      email,
+      password,
+      phone,
+      gender,
+      isActive,
     });
     return res.status(200).json({ user });
   } catch (error) {
