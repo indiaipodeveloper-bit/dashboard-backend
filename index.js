bad6d41eb15d82569b6c0569378735d9c5ae2145
IPO <indiaipodeveloper@gmail.com>
2025-10-16 15:25:36 +0530

    adfs

--- index.js
+++ index.js
@@ -1,64 +1,14 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import { connect } from "./config/dbconnection.js";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import { AdminRouter } from "./routes/Routes.js";
-// import { UserRouter } from "./routes/UserRoutes.js";
-// dotenv.config();
-
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// app.use(
-//   cors({
-//     origin: "https://dashboard-frontend-smoky.vercel.app",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-//   })
-// );
-
-// app.use(cookieParser());
-// app.use(express.json());
-
-
-// app.use("/uploads/profiles", express.static("uploads/profiles"));
-
-
-// app.use("/admin",AdminRouter)
-// app.use("/",UserRouter)
-
-// app.get("/",(req,res)=>{
-//   return res.send("dashboard backend")
-// })
-
-// console.log(process.env.MONGOURI)
-// connect(process.env.MONGOURI)
-
-//   .then(() => {
-//     console.log("db connected");
-//     app.listen(PORT, () => {
-//       console.log("App runnin on PORT", PORT);
-//     });
-//   })
-//   .catch((er) => {
-//     console.log(er);
-//   });
-
-
-
 import express from "express";
 import dotenv from "dotenv";
+import { connect } from "./config/dbconnection.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { AdminRouter } from "../routes/Routes.js";
-import { UserRouter } from "../routes/UserRoutes.js";
-import serverless from "serverless-http";
-import { connect } from "./config/dbconnection.js";
-
+import { AdminRouter } from "./routes/Routes.js";
+import { UserRouter } from "./routes/UserRoutes.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
@@ -70,17 +20,28 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+
+
 app.use("/uploads/profiles", express.static("uploads/profiles"));
 
-app.use("/admin", AdminRouter);
-app.use("/", UserRouter);
 
-app.get("/", (req, res) => {
-  res.send("Dashboard backend running on Vercel 🚀");
-});
+app.use("/admin",AdminRouter)
+app.use("/",UserRouter)
 
-await connect(process.env.MONGOURI)
-  .then(() => console.log("MongoDB connected ✅"))
-  .catch((err) => console.log("MongoDB error ❌", err));
+app.get("/",(req,res)=>{
+  return res.send("dashboard backend")
+})
 
-export const handler = serverless(app);
+console.log(process.env.MONGOURI)
+connect(process.env.MONGOURI)
+
+  .then(() => {
+    console.log("db connected");
+    app.listen(PORT, () => {
+      console.log("App runnin on PORT", PORT);
+    });
+  })
+  .catch((er) => {
+    console.log(er);
+  });
+
